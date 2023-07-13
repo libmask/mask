8bb1b9aac9c9408168528bd1c596e18bc56dbcc4
Christoffer Hallas <hallas@libewd.com>
2023-07-12 22:15:39 -0400

    fix: use www as root

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   root: './www',
-  outDir: '../dist',
   resolve: {
     alias: {
       'libmask/mask': `${__dirname}/src/index`,
