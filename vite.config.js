1c845e1d1cc723a7f31e66bef6341fcc65fceace
Christoffer Hallas <hallas@libewd.com>
2023-07-12 22:08:06 -0400

    fix: configured vite build

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   root: './www',
+  outDir: '../dist',
   resolve: {
     alias: {
       'libmask/mask': `${__dirname}/src/index`,
